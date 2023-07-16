577451fe3da50292aad3d5aa6d253971fe607b9b
Mohamed SMATI <smatimed-github@yahoo.com>
2023-07-16 04:14:10 +0100

    pb reglé de statcounter (ce n'était pas ça, c'est plutot https)

--- _create_ZIP (without DB).cmd
+++ _create_ZIP (without DB).cmd
@@ -2,6 +2,8 @@
 
 set /P addToHangman=Additif to 'Hangman.zip': 
 
+xcopy static static_production /S /Y
+
 D:\Tools\Winrar\WINRAR.exe a Hangman%addToHangman%.zip -r @_create_Zip_without_DB.txt
 
 timeout 10
--- _create_ZIP.cmd
+++ _create_ZIP.cmd
@@ -2,6 +2,8 @@
 
 set /P addToHangman=Additif to 'Hangman.zip': 
 
+xcopy static static_production /S /Y
+
 D:\Tools\Winrar\WINRAR.exe a Hangman%addToHangman%.zip -r @_create_Zip.txt
 
 timeout 10
