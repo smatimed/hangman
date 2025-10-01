5b8dc1a354b921b707cf97e30ecee9a83582fbba
Mohamed Smati <smati_med@yahoo.com>
2025-10-01 16:21:51 +0100

    rajout de 'mylinks'

--- hangman/urls.py
+++ hangman/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    # projet "mylinks"
+    path("mylinks/", TemplateView.as_view(template_name="mylinks/index.html")),
 ]
